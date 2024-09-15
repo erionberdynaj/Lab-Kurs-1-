860010279a1ce35201906ea16fa2ea100fcc5f2c
Erion Berdynaj <erion.berdyna@hotmail.com>
2024-09-15 20:02:37 +0200

    Implementimi i aseteve

---
+++ carsystem/assets/data/serviceData.js
@@ -0,0 +1,45 @@
+const serviceData = [
+  {
+    id: 1,
+    title: "City Transfer",
+    icon: "ri-map-pin-2-line",
+    desc: "Our commitment to safety starts with a meticulously maintained fleet of vehicles, each undergoing rigorous inspections to meet the highest industry standards.",
+  },
+
+  {
+    id: 2,
+    title: "Whole  City Tour",
+    icon: "ri-community-line",
+    desc: "Our commitment to safety starts with a meticulously maintained fleet of vehicles, each undergoing rigorous inspections to meet the highest industry standards.",
+  },
+
+  {
+    id: 3,
+    title: "Unlimited Miles Car Rental",
+    icon: "ri-roadster-line",
+    desc: "Our commitment to safety starts with a meticulously maintained fleet of vehicles, each undergoing rigorous inspections to meet the highest industry standards.",
+  },
+
+  {
+    id: 4,
+    title: "Fast & Easy Booking",
+    icon: "ri-timer-flash-line",
+    desc: "Our commitment to safety starts with a meticulously maintained fleet of vehicles, each undergoing rigorous inspections to meet the highest industry standards.",
+  },
+
+  {
+    id: 5,
+    title: "Many Pickup Locations",
+      icon: "ri-map-pin-line",
+      desc: "Our commitment to safety starts with a meticulously maintained fleet of vehicles, each undergoing rigorous inspections to meet the highest industry standards.",
+  },
+
+  {
+    id: 6,
+    title: "Airport Transfer",
+    icon: "ri-flight-takeoff-line",
+    desc: "Our commitment to safety starts with a meticulously maintained fleet of vehicles, each undergoing rigorous inspections to meet the highest industry standards.",
+  },
+];
+
+export default serviceData;
