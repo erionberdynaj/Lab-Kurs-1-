179d4a15f14d2ee17e44b1b308ebb0c61e4aff87
Erion Berdynaj <erion.berdyna@hotmail.com>
2024-09-16 17:55:18 +0200

    vendosja e disa klasave tek UI

---
+++ carsystem/components/UI/Testimonial.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Slider from "react-slick";
+
+import "../../styles/testimonial.css";
+
+import ava01 from "../../assets/all-images/ava-1.jpg";
+import ava02 from "../../assets/all-images/ava-2.jpg";
+import ava03 from "../../assets/all-images/ava-3.jpg";
+import ava04 from "../../assets/all-images/ava-4.jpg";
+
+const Testimonial = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1000,
+    swipeToSlide: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+   
+  };
+
+  return (
+    <Slider {...settings}>
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        I've been using Car Rental Company for my travel needs for quite some time. The level of service is very highly. I recommend!
+        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava01} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        I've been using Car Rental Company for my travel needs for quite some time. The level of service is very highly. I recommend!        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava02} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        I've been using Car Rental Company for my travel needs for quite some time. The level of service is very highly. I recommend!        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava03} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="testimonial py-4 px-3">
+        <p className="section__description">
+        I've been using Car Rental Company for my travel needs for quite some time. The level of service is very highly. I recommend!        </p>
+
+        <div className="mt-3 d-flex align-items-center gap-4">
+          <img src={ava04} alt="" className="w-25 h-25 rounded-2" />
+
+          <div>
+            <h6 className="mb-0 mt-3">Jhon Doe</h6>
+            <p className="section__description">Customer</p>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
+};
+
+export default Testimonial;
